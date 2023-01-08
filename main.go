16f91236429ce2cd9c2ef2ea8349235a0f50a611
jmnote <opcore@gmail.com>
2023-01-08 17:56:04 +0900

    This should be a constant

--- main.go
+++ main.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
-	fmt.Println("hello world")
-	fmt.Println("hello world")
+	prepare("This should be a constant") // Noncompliant; 'This should ...' is duplicated 3 times
+	execute("This should be a constant")
+	release("This should be a constant")
+}
+
+func prepare(s string) {
+	fmt.Println(s)
+}
+func execute(s string) {
+	fmt.Println(s)
+}
+func release(s string) {
+	fmt.Println(s)
 }
