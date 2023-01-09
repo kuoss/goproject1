269d4a525b9715fc83a497f5415f31833af9746b
Jmnote <opcore@gmail.com>
2023-01-09 17:04:48 +0900

    Update main.go

--- main.go
+++ main.go
@@ -5,3 +5,13 @@ import "fmt"
 func main() {
 	fmt.Println("hello world")
 }
+
+// Add is our function that sums two integers
+func Add(x, y int) (res int) {
+	return x + y
+}
+
+// Subtract subtracts two integers
+func Subtract(x, y int) (res int) {
+	return x - y
+}
