bc13edc7eba6afde1f4c402b7b0335e85aa4c821
Jmnote <opcore@gmail.com>
2023-01-11 01:54:25 +0900

    Update main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world1")
-	fmt.Println("hello world")
+	fmt.Println("hello world2")
 	fmt.Println("hello world")
 	fmt.Println("hello world")
 	fmt.Println("hello world")
