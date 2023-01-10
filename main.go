0b03c8f13ced0aa86606d9d2dea43b4e77c10dba
Jmnote <opcore@gmail.com>
2023-01-11 01:48:28 +0900

    Update main.go

--- main.go
+++ main.go
@@ -9,8 +9,6 @@ func main() {
 	fmt.Println("hello world")
 	fmt.Println("hello world")
 	fmt.Println("hello world")
-	fmt.Println("hello world")
-	fmt.Println("hello world")
 }
 
 // Add is our function that sums two integers
