2300ffe1630cbe673abf51cedbb1af820593a803
Jmnote <opcore@gmail.com>
2023-01-11 18:34:47 +0900

    Update main.go

--- main.go
+++ main.go
@@ -4,14 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world1")
-	fmt.Println("hello world2")
-	fmt.Println("hello world3")
-	fmt.Println("hello world4")
-	fmt.Println("hello world5")
-	fmt.Println("hello world6")
-	fmt.Println("hello world7")
-	fmt.Println("hello world8")
-	fmt.Println("hello world9")
 }
 
 // Add is our function that sums two integers
